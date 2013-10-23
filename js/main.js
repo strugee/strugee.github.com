ce60c1eefb0c82e92856b7f1453ccbe409ba9316
Alex Jordan <alexander3223098@gmail.com>
2013-10-23 16:48:11 +0100

    add night mode

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+$(document).ready(function(){
+	// Register an event handler for the lightbulb
+	$('#lightbulb-icon').click(nightmode);
+});
 
+function nightmode() {
+	$('head').append('<link id="nightmode-stylesheet" rel="stylesheet" type="text/css" href="css/nightmode.css" />');
+	$('#lightbulb-icon').unbind('click').click(daymode);
+	$('#lightbulb-icon').attr("href", "#lightbulb-icon");
+	$('#lightbulb-icon').attr("alt", "Lightbulb icon (white)");
+	console.log("welcome to night mode, you beautiful person!");
+}
+	
+function daymode() {
+	$('#nightmode-stylesheet').remove();
+	$('#lightbulb-icon').unbind('click').click(nightmode);
+	$('#lightbulb-icon').attr("href", "#lightbulb-icon");
+	$('#lightbulb-icon').attr("alt", "Lightbulb icon");
+	console.log("let the sun shine upon all! welcome to day mode.");
+}
