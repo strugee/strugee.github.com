45115e617afac4e71f5ba7e4a8ae1abee22ba4a7
Alex Jordan <alexander3223098@gmail.com>
2013-10-23 08:53:58 -0700

    fix a nightmode bug
    this fixes a bug in Chrome where when you click the lightbulb icon, it
    will jump to the bottom of the page.

--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@ $(document).ready(function(){
 function nightmode() {
 	$('head').append('<link id="nightmode-stylesheet" rel="stylesheet" type="text/css" href="css/nightmode.css" />');
 	$('#lightbulb-icon').unbind('click').click(daymode);
-	$('#lightbulb-icon').attr("href", "#lightbulb-icon");
-	$('#lightbulb-icon').attr("alt", "Lightbulb icon (white)");
+	$('#lightbulb-icon').attr("href", "javascript:void(0);");
+	$('#lightbulb-icon').attr("alt", "Lightbulb icon");
 	console.log("welcome to night mode, you beautiful person!");
 }
 	
 function daymode() {
 	$('#nightmode-stylesheet').remove();
 	$('#lightbulb-icon').unbind('click').click(nightmode);
-	$('#lightbulb-icon').attr("href", "#lightbulb-icon");
+	$('#lightbulb-icon').attr("href", "javascript:void(0);");
 	$('#lightbulb-icon').attr("alt", "Lightbulb icon");
 	console.log("let the sun shine upon all! welcome to day mode.");
 }
