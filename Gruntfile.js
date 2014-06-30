c89cc73711a456b078c58b27258527abe528a84c
Alex Jordan <alexander3223098@gmail.com>
2014-06-30 14:46:19 -0700

    properly quote Grunt config keys

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,16 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		jshint: {
-			options: {
-				force: true
+		'pkg': grunt.file.readJSON('package.json'),
+		'jshint': {
+			'options': {
+				'force': true
 			},
-			all: ['Gruntfile.js', 'js/*.js']
+			'all': ['Gruntfile.js', 'js/*.js']
 		},
 		
-		csslint: {
-			lax: {
-				src: ['css/main.css', 'css/nightmode.css', 'css/ie9.css']
+		'csslint': {
+			'lax': {
+				'src': ['css/main.css', 'css/nightmode.css', 'css/ie9.css']
 			}
 		}
 		
