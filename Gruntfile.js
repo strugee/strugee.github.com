3022f951ee4fbc6ce9c6c31733f508414aa2befc
Alex Jordan <alexander3223098@gmail.com>
2014-06-30 15:29:33 -0700

    add experimental Grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,14 +12,45 @@ module.exports = function(grunt) {
 			'lax': {
 				'src': ['css/main.css', 'css/nightmode.css', 'css/ie9.css']
 			}
+		},
+		'static_handlebars': {
+			'options':
 		}
-		
-		
+		/*
+		'template': {
+			'options': {
+				
+			},
+			'main-site': {
+				'options': {
+					
+				},
+				'files': {
+					'dist/index.html': ['src/index.html.tpl']
+				}
+			}
+		}
+		*/
 	});
 	
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-static-handlebars');
 	
-	grunt.registerTask('lint', ['jshint', 'csslint']);
-	grunt.registerTask('default', ['lint']);
+	grunt.registerTask('lint',
+		[
+		'jshint',
+		'csslint'
+		]
+	);
+	grunt.registerTask('build',
+		[
+		'static-handlebars'
+		]
+	);
+	grunt.registerTask('default',
+		[
+		'lint'
+		]
+	);
 };
