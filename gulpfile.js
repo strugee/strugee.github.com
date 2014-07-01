15fd9a579082d6c58a9aee075e56e24a52fa87ee
Alex Jordan <alexander3223098@gmail.com>
2014-07-01 01:13:29 -0700

    render using Handlebars

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 var gulp = require('gulp');
+var build = require('gulp-build');
+
+var options = {
+	partials: [{
+		name: 'footer',
+		tpl: 'something'
+	}]
+}
+
+gulp.task('build', function() {
+	gulp.src('./index.html')
+	    .pipe(build({}, options))
+	    .pipe(gulp.dest('dist/'));
+});
 
 gulp.task('default', function() {
-	// TODO
+	
 });
