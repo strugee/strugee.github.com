25e1f301220f89378ef85d3e1b26df3d6c3475f7
Alex Jordan <alexander3223098@gmail.com>
2014-07-14 15:09:06 -0700

    cache-bust CSS with gulp-rev

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
+var rev = require('gulp-rev')
 
 /* Regular tasks */
 
 /* TODO: validate HTML */
+
+gulp.task('build', function() {
+	gulp.src(['css/*.css'])
+	    .pipe(rev())
+	    .pipe(gulp.dest('dist/css'))
+	    .pipe(rev.manifest())
+	    .pipe(gulp.dest('dist/css'));
+});
 
 gulp.task('csslint', function() {
 	
@@ -23,6 +32,6 @@ gulp.task('lint', ['csslint', 'jshint'], function() {
 
 /* Default task */
 
-gulp.task('default', [, 'lint'], function() {
+gulp.task('default', ['build', 'lint'], function() {
 	
 });
