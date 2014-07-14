4c6f188310314733bf8cf539baf23d82bbbf4efe
Alex Jordan <alexander3223098@gmail.com>
2014-07-14 16:02:09 -0700

    add WIP build system
    still TODO: Wintersmith integration

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,34 @@
 var gulp = require('gulp');
 
+var filter = require('gulp-filter');
 var jshint = require('gulp-jshint');
-var rev = require('gulp-rev')
+var rev = require('gulp-rev');
+var revReplace = require('gulp-rev-replace');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var csso = require('gulp-csso');
 
 /* Regular tasks */
 
 /* TODO: validate HTML */
 
 gulp.task('build', function() {
-	gulp.src(['css/*.css'])
-	    .pipe(rev())
-	    .pipe(gulp.dest('dist/css'))
-	    .pipe(rev.manifest())
-	    .pipe(gulp.dest('dist/css'));
+	var jsFilter = filter('**/*.js');
+	var cssFilter = filter('**/*.css');
+	
+	gulp.src('index.html')
+	    .pipe(useref.assets())      // Concatenate with gulp-useref
+	    .pipe(jsFilter)
+	    .pipe(uglify())             // Minify any javascript sources
+	    .pipe(jsFilter.restore())
+	    .pipe(cssFilter)
+	    .pipe(csso())               // Minify any CSS sources
+	    .pipe(cssFilter.restore())
+	    .pipe(rev())                // Rename the concatenated files
+	    .pipe(useref.restore())
+	    .pipe(useref())
+	    .pipe(revReplace())         // Substitute in new filenames
+	    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('csslint', function() {
